14dfb749389f422f43a411db629589c850612040
Paresh Sharma <43784445+Paresh95@users.noreply.github.com>
2021-07-05 14:21:34 +0100

    Revert "Updated for division"

--- testing.py
+++ testing.py
@@ -3,17 +3,3 @@ def add(x, y):
 
 
 print(add(1, 2))
-
-
-def division(x, y):
-    return x / y
-
-
-print(division(1, 2))
-
-
-def subtraction(x, y):
-    return x - y
-
-
-print(subtraction(1, 2))
